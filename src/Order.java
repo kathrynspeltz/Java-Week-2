561f2c26ff78ef797a15e2f5b50b98f5f2bb14cf
kathrynspeltz <kathrynspeltz@gmail.com>
2023-08-23 14:32:23 -0500

    Mid week updates

---
+++ src/Order.java
@@ -0,0 +1,80 @@
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Order{
+
+    public Order(ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu){
+        System.out.println("Hello customer. Would you like to place an order? (Y or N)");
+        Scanner input = new Scanner(System.in);
+        String placeOrder = input.nextLine();
+
+        ArrayList<Object> order = new ArrayList<Object>();
+
+        if (placeOrder.equalsIgnoreCase("Y")) {
+            order.add(LocalDate.now());
+            order.add(LocalTime.now());
+            System.out.println("Here is the menu");
+            System.out.println("CUPCAKES");
+            int itemNumber = 0;
+
+            for (int i = 0; i < cupcakeMenu.size(); i++) {
+                itemNumber += 1;
+                System.out.println(itemNumber + ".");
+                cupcakeMenu.get(i).type();
+                System.out.println("Price: $" + cupcakeMenu.get(i).getPrice());
+                System.out.println();
+            }
+            System.out.println("DRINKS");
+            for (int j = 0; j < drinkMenu.size(); j++){
+                itemNumber += 1;
+                System.out.println(itemNumber + ".");
+                drinkMenu.get(j).type();
+                System.out.println("Price: $" + drinkMenu.get(j).getPrice());
+                System.out.println();
+            }
+
+            boolean ordering = true;
+
+            while (ordering){
+                System.out.println("What would you like to order? Please use the number associated with each item to order.");
+                int orderChoice = input.nextInt();
+                input.nextLine();
+                if (orderChoice == 1){
+                    order.add(cupcakeMenu.get(0));
+                    System.out.println("Item added to order");
+                } else if (orderChoice == 2){
+                    order.add(cupcakeMenu.get(1));
+                    System.out.println("Item added to order");
+                } else if (orderChoice == 3){
+                    order.add(cupcakeMenu.get(2));
+                    System.out.println("Item added to order");
+                } else if (orderChoice == 4){
+                    order.add(drinkMenu.get(0));
+                    System.out.println("Item added to order");
+                } else if (orderChoice == 5){
+                    order.add(drinkMenu.get(1));
+                    System.out.println("Item added to order");
+                } else if (orderChoice == 6){
+                    order.add(drinkMenu.get(2));
+                    System.out.println("Item added to order");
+                } else {
+                    System.out.println("Sorry, we don’t seem to have that on the menu.");
+                }
+                System.out.println("Would you like to continue ordering? (Y/N)");
+                String continueOrder = input.nextLine();
+                if (!continueOrder.equalsIgnoreCase("Y")){
+                    ordering = false;
+                }
+            }
+
+        } else {
+            System.out.println("Have a nice day then");
+        }
+
+    }
+
+
+}
